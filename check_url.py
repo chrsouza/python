ca3f5ed09c8da7ef7e56908e6fc6a9cea2fff775
Losjack <losjack@localhost.localdomain>
2020-06-14 15:10:18 -0300

    first commit

---
+++ check_url.py
@@ -0,0 +1,15 @@
+import requests 
+
+def url_ok(url):
+    r = requests.head(url)
+    return r.status_code == 200
+
+def url_statuscode(url):
+    r = requests.head(url)
+    return r.status_code
+
+url = "https://www.uol.com.br" 
+#print(url_ok("https://www.uol.com.br"))
+
+print("URL: ", url)
+print("Status code: ", url_statuscode(url))
